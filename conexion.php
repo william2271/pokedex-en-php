7c1fd3dbb9ee968d3b64ffd0862080ad3a6bb300
unknown <William22711@gmail.com>
2020-07-21 16:11:26 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,11 @@
 <?php
-$password = "";
-  $conexion = new mysqli("localhost","root",$password,"typen");
-  mysqli_select_db("pokedex",$conexion);
+$servidor = "localhost";
+$usuario = "root";
+$conexion = mysqli_connect( $servidor, $usuario, "" );
+
+  mysqli_select_db($conexion,"pokedex");
   
   $mysqli->set_charset("utf8");
   
-  //date_default_timezone_set("America/Mexico");
-  /*
-  function limpiar($tags){
-    $tags = strip_tags($tags);
-    $tags = stripslashes($tags);
-    $tags = htmlentities($tags);
-    $tags = mysql_real_escape_string($tags);
-    return $tags;
-  }
-  */
+
 ?>
